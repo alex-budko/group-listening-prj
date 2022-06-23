b24916a91becf7fb9128f3516c9ca0b5fa4f4325
alex-budko <xalex.budko@gmail.com>
2022-06-23 17:38:26 -0400

    handling POST requests

--- group_listening_prj/api/urls.py
+++ group_listening_prj/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.RoomView.as_view(), name='room-view')
+    path('', views.RoomView.as_view(), name='room-view'),
+    path('create-room/', views.CreateRoomView.as_view(), name='creat-room-view')
 ]
