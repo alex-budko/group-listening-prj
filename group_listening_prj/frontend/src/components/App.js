e82c3c567eae6ea09d5e6145a1331a52961c50d0
alex-budko <xalex.budko@gmail.com>
2022-06-23 17:19:18 -0400

    router changes

--- group_listening_prj/frontend/src/components/App.js
+++ group_listening_prj/frontend/src/components/App.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react'
 import { render } from 'react-dom'
+import CreateRoomPage from './CreateRoomPage';
+import HomePage from './HomePage';
+import RoomJoinPage from './RoomJoinPage';
 
 export default class App extends Component  {
     constructor(props) {
         super(props);
+        this.state = {
+            
+        }
     }
     render() {
-        return <h1>Testing React Code</h1>
+        return <div>
+            <HomePage />
+        </div>
     }
 }
 
 const appDiv = document.getElementById("app");
-render(<App />, appDiv)
+render(<App name='tim' />, appDiv)
--- group_listening_prj/frontend/urls.py
+++ group_listening_prj/frontend/urls.py
@@ -4,4 +4,6 @@ from .views import index
 
 urlpatterns = [
     path('', index, name='index'),
+    path('join/', index, name='join'),
+    path('create/', index, name='create'),
 ]
